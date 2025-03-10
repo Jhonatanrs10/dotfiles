62c0a63a5525632210cc58bc38ad0ec35dd31c17
jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-03-10 16:57:16 -0300

    att

---
+++ jrs/.config/jrs/colors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Definição de cores
+export JRS_MAIN_COLOR="#1793d1"
+export JRS_BAR_COLOR="#333333"
+export JRS_TEXT_COLOR="#ffffff"
+export JRS_UNFOCUSED_COLOR="#7d7d7d"
+export JRS_BAD_COLOR="#900000"
+export JRS_DEGRADED_COLOR="#a08000"
+export JRS_WHITE_COLOR="#ffffff"
+export JRS_BLACK_COLOR="#000000"
+
+
+#i3wm $env:VARIAVEL
+
+#hyprland $env:VARIAVEL
+
+#rofi @import "~/.cache/colors.sh"
+#* {
+#    background: @BG_COLOR;
+#    foreground: @FG_COLOR;
+#    border-color: @BORDER_COLOR;
+#}
+
+#wofi $env:VARIAVEL
+
+#waybar $env:VARIAVEL
+
+#polybar ${env:VARIAVEL}
+
---
+++ stow.sh
@@ -0,0 +1,46 @@
+echo "[1]STOW ALL [2]UNSTOW ALL"
+read resp
+case $resp in
+1)
+stow -t ~ alacritty
+stow -t ~ bspwm
+stow -t ~ dunst
+stow -t ~ fastfetch
+stow -t ~ hypr
+stow -t ~ i3
+stow -t ~ i3status
+stow -t ~ jrs
+sudo stow -t /etc/lightdm/ lightdm
+stow -t ~ mangohud
+stow -t ~ nvim
+stow -t ~ picom
+stow -t ~ polybar
+stow -t ~ rofi
+stow -t ~ shortcuts
+stow -t ~ sxhkd
+stow -t ~ waybar
+stow -t ~ wofi
+;;
+2)
+stow -D -t ~ alacritty
+stow -D -t ~ bspwm
+stow -D -t ~ dunst
+stow -D -t ~ fastfetch
+stow -D -t ~ hypr
+stow -D -t ~ i3
+stow -D -t ~ i3status
+stow -D -t ~ jrs
+sudo stow -D -t /etc/lightdm/ lightdm
+stow -D -t ~ mangohud
+stow -D -t ~ nvim
+stow -D -t ~ picom
+stow -D -t ~ polybar
+stow -D -t ~ rofi
+stow -D -t ~ shortcuts
+stow -D -t ~ sxhkd
+stow -D -t ~ waybar
+stow -D -t ~ wofi
+;;
+*)
+;;
+esac
