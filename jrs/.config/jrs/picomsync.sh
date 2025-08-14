2ad690191e21200f7a940d31209cbb2e6011c396
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-08-14 19:42:07 -0300

    att

--- jrs/.config/jrs/picomsync.sh
+++ jrs/.config/jrs/picomsync.sh
@@ -4,9 +4,11 @@ case $notifyValueNow in
   "vsync = true;")
     sed -i s/"vsync = true;"/"vsync = false;"/g "$HOME/.config/picom/picom.conf" 
     dunstify -t 1000 --hints int:transient:1 "Picom" "VSync: Off" --icon=picom
+    nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
     ;;
   *)
     sed -i s/"vsync = false;"/"vsync = true;"/g "$HOME/.config/picom/picom.conf" 
     dunstify -t 1000 --hints int:transient:1 "Picom" "VSync: On" --icon=picom
+    nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=Off}"
     ;;
 esac
