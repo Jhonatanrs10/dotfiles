213779e793ac4d112bea80f4bdd1f8fb82917145
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-09-06 13:41:16 -0300

    att

--- bashmenu/.config/bashmenu/scripts/steamdecklike.sh
+++ bashmenu/.config/bashmenu/scripts/steamdecklike.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 setupSteamDeckLike(){
-        echo "SteamDeckLike
+    SESSION_NAME="SteamDecklike"
+    FILENAME="${SESSION_NAME}.desktop"
+    EXEC_COMMAND="$mySteamDeckLike"
+    SESSION_DIR="/usr/share/xsessions"
+    echo "SteamDeckLike
 [1]Criar Sessão, [2]Remover Sessão"
     read resp
     case $resp in
 		1)
-            SESSION_NAME="SteamDeck-like"
-            FILENAME="${SESSION_NAME}.desktop"
-            EXEC_COMMAND="$mySteamDeckLike"
-            SESSION_DIR="/usr/share/xsessions"
+            
             sudo mkdir -p "$SESSION_DIR"
             cat <<EOF | sudo tee "${SESSION_DIR}/${FILENAME}"
 [Desktop Entry]
 Name=${SESSION_NAME}
 Comment=Uma sessão de games com aparência de Steam Deck.
 Exec=${EXEC_COMMAND}
+Icon=steam
 Type=Application
 EOF
             sudo chmod 644 "${SESSION_DIR}/${FILENAME}"
+            cp $SESSION_DIR/$FILENAME $HOME/.local/share/applications/
             echo "Arquivo de sessão criado com sucesso em: ${SESSION_DIR}/${FILENAME}"
             echo "A nova sessão '${SESSION_NAME}' agora está disponível no seu display manager.";;
         2)
             sudo rm $SESSION_DIR/$FILENAME
-            echo "Arquivo de sessão removido com sucesso de: ${SESSION_DIR}";;
+            sudo rm $HOME/.local/share/applications/$FILENAME
+            echo "Arquivos removidos.";;
 		*)
 	esac
 
