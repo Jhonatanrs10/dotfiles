325cde79676ec4857ae30f40e9ac293674233eaa
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-09-07 12:58:54 -0300

    att

--- bashmenu/.config/bashmenu/scripts/steamdecklike.sh
+++ bashmenu/.config/bashmenu/scripts/steamdecklike.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+# DOCs
+#https://github.com/ValveSoftware/gamescope/issues/1800
+#MANGOHUD_CONFIG="read_cfg" MANGOHUD_CONFIGFILE="$HOME/.config/MangoHud/MangoHud.conf" gamescope --adaptive-sync --hdr-enabled -W 3840 -H 2160 -f --steam --mangoapp -- /usr/bin/steam -gamepadui
+
+#https://github.com/shahnawazshahin/steam-using-gamescope-guide/wiki/Steam-using-Gamescope-guide-%E2%80%90-Wiki-page
+#gamescope -e -- steam -steamdeck -steamos3
+
+#ARCHWIKI
+#steam gamescope mangohud gamemode
+
 setupSteamDeckLike(){
     SESSION_NAME="SteamDecklike"
     FILENAME="${SESSION_NAME}.desktop"
     EXEC_COMMAND="gamescope -W 1600 -H 900 -w 1600 -h 900 -r 60 -f -C 5000 -e --force-grab-cursor --mangoapp -- steam -steamdeck -steamos3"
+    APP_DIR="$HOME/.local/share/applications"
     XSESSION_DIR="/usr/share/xsessions"
     WSESSION_DIR="/usr/share/wayland-sessions"
-    APP_DIR="$HOME/.local/share/applications"
     echo "SteamDeckLike
-[1]Criar Sessão, [2]Remover Sessão"
+[1]Criar Sessão, [2]Remover Sessão [3]Dependencias"
     read resp
     case $resp in
 		1)
-            
+            sudo mkdir -p "$APP_DIR"
             sudo mkdir -p "$XSESSION_DIR"
             sudo mkdir -p "$WSESSION_DIR"
-            sudo mkdir -p "$APP_DIR"
-            cat <<EOF | sudo tee "${XSESSION_DIR}/${FILENAME}"
+            cat <<EOF | sudo tee "${APP_DIR}/${FILENAME}"
 [Desktop Entry]
 Name=${SESSION_NAME}
 Comment=Uma sessão de games com aparência de Steam Deck.
@@ -34,6 +43,7 @@ EOF
             sudo rm $WSESSION_DIR/$FILENAME
             sudo rm $APP_DIR/$FILENAME
             echo "Arquivos removidos.";;
+        3)packagesManager "$myBaseSteamDeckLike";;
 		*)
 	esac
 
