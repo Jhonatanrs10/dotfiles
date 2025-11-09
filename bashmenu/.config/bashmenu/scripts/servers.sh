4e0a7983d3ea61f4b63bd4884ee796724bc7ebc8
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-11-09 15:43:58 -0300

    att

--- bashmenu/.config/bashmenu/scripts/servers.sh
+++ bashmenu/.config/bashmenu/scripts/servers.sh
@@ -264,38 +264,16 @@ cd $diretorioInstall/*/Linux
 }   
 
 installUnturnedServer(){
-    #cria e loga no usuario steam
-    #criaLogaUserSteam
 
     installName="UnturnedServer"
     uninstallPastaAtalhoBinMesmoNome "$installName"
     criaDiretorioInstall "$dBashMenu/$installName"
-
-    aptSteamCMD(){
-        #dependencias
-        sudo add-apt-repository multiverse
-        sudo apt install software-properties-common
-        sudo dpkg --add-architecture i386
-        sudo apt update
-        #instalando SteamCMD
-        sudo apt install lib32gcc-s1 steamcmd
-        #baixando dependencias
-        steamcmd +login anonymous +app_update 1110390 +quit
-    }
-    
-    manuallySteamCMD(){
-        packagesManager "lib32gcc-s1"
-        criaPastaBaixaExtrai "$diretorioInstall" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" "steamcmd.tar.gz"
-        #baixando dependencias
-        bash ./steamcmd.sh +login anonymous +app_update 1110390 +quit
-        packagesManager "tmux screen"
-    }
     
     criarArq "Map Ex.(PEI - Germany - Russia - Washington) 
 help no console do server para ver comandos
 edite o local/diretorio do server em run.sh
 mova ou copie o arquivo commands.dat para:
-Steam/steamapps/common/U3DS/Servers/JardimRecreio/Server/Commands.dat " "LEIAME.txt"
+Steam/steamapps/common/U3DS/Servers/JardimRecreio/Server/Commands.dat" "$diretorioInstall/LEIAME.txt"
 
     criarArq "Name JardimRecreio
 Map PEI
@@ -305,17 +283,14 @@ perspective both
 mode normal
 pve
 welcome Bem Vindo ao bairro!!
-cheats on" "Commands.dat"
-
-    menu12345 "[1]APT [2]MANUALLY" "aptSteamCMD" "manuallySteamCMD"
+cheats on" "$diretorioInstall/Commands.dat"
 
     criarArq "#!/usr/bin/env sh
-bash $diretorioInstall/steamcmd.sh +login anonymous +app_update 1110390 +quit
+steamcmd +login anonymous +app_update 1110390 +quit
 cd $HOME/Steam/steamapps/common/U3DS
 bash ServerHelper.sh +LanServer/JardimRecreio" "$diretorioInstall/run.sh"
 
-    #criaAtalho "$installName" "Description" "bash run.sh" "$diretorioInstall" "false" "$installName" "$dBashMenu/Icons/default.svg"
-    criaAtalhoBin "$diretorioInstall/run.sh" "$installName"
+    #criaAtalhoBin "$diretorioInstall/run.sh" "$installName"
 
 }   
 
@@ -326,7 +301,7 @@ installProjectZomboidServer(){
     installSteamCMD
 
     criarArq "#!/bin/sh
-steamcmd +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +force_install_dir "$diretorioInstall" +login anonymous +app_update 380870 validate +quit" "$diretorioInstall/update_zomboid.txt"
+steamcmd +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +force_install_dir "$diretorioInstall" +login anonymous +app_update 108600 validate +quit" "$diretorioInstall/update_zomboid.txt"
 
 criarArq "export PATH=$PATH:/usr/games
 steamcmd +runscript $HOME/update_zomboid.txt" "run.sh"
