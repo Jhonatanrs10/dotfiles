a1797eb2a50b7c75c51f21af8d963ba33bb7abe8
jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-11-13 15:26:14 -0300

    att

--- bashmenu/.config/bashmenu/scripts/debianBase.sh
+++ bashmenu/.config/bashmenu/scripts/debianBase.sh
@@ -7,7 +7,6 @@ addArchi386(){
 aptUpdate(){
 	echo -e "[INFO] - ATUALIZANDO REPOSITORIO E FAZENDO ATUALIZACAO DO SISTEMA - [INFO]"
 	sudo apt update && sudo apt dist-upgrade -y
-	sudo pacman -Syyuu
 }
 
 installDebs(){
--- bashmenu/.config/bashmenu/scripts/posInstall.sh
+++ bashmenu/.config/bashmenu/scripts/posInstall.sh
@@ -13,7 +13,6 @@ myBasePosInstall(){
     sambaSetup
     configsSetup
     desktopSetup
-    
 }
 
 pacmanSetup(){
