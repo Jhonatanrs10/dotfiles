658faa93dd78f0c7eb9e801e20d7274363ad7f53
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2025-11-27 19:26:42 -0300

    att

--- hypr/.config/hypr/hyprpaper.sh
+++ hypr/.config/hypr/hyprpaper.sh
@@ -1,2 +1,2 @@
-hyprctl hyprpaper preload ~/.config/wallpapers/w3.png
-hyprctl hyprpaper wallpaper , ~/.config/wallpapers/w3.png
+hyprctl hyprpaper preload ~/.config/wallpapers/w9.png
+hyprctl hyprpaper wallpaper , ~/.config/wallpapers/w9.png
