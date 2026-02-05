cdf9e90d91a1a8b061cb84fa704c62dc61f01c82
jhonatanrs10 <jhonatanrs10@hotmail.com>
2026-02-05 10:49:17 -0300

    att

--- jrs/.config/jrs/jrs-rofi-display-resolutions.sh
+++ jrs/.config/jrs/jrs-rofi-display-resolutions.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-nwg_displays_480p() {
+nwg_displays_low_resolution() {
 	echo "monitor=eDP-1,854x480@60.02,0x0,1.0" >$HOME/.config/hypr/monitors.conf
 	source $HOME/.config/jrs/jrs-reload-wm.sh
 }
 
-nwg_displays_768p() {
+nwg_displays_default_resolution() {
 	echo "monitor=eDP-1,1366x768@60.02,0x0,1.0" >$HOME/.config/hypr/monitors.conf
 	source $HOME/.config/jrs/jrs-reload-wm.sh
 }
 
-xrandr_720p(){
-	xrandr --output DP-0 --mode 1280x720 --pos 1280x0 --rotate normal
+xrandr_low_resolution(){
+	xrandr --output eDP-1 --mode 864x486 --pos 1280x0 --rotate normal
 }
 
-xrandr_900p(){
-	xrandr --output DP-0 --mode 1600x900 --pos 1280x0 --rotate normal
+xrandr_default_resolution(){
+	xrandr --output eDP-1 --mode 1366x768 --pos 1280x0 --rotate normal
 }
 
 if [ "$XDG_CURRENT_DESKTOP" = "i3" ] || [ "$XDG_CURRENT_DESKTOP" = "bspwm" ]; then
-	options="eDP-0 720p\neDP-0 900p"
+	options="eDP-1 Default ResX\neDP-1 Low ResX"
 else
-	options="eDP-1 480p\neDP-1 768p"
+	options="eDP-1 Default ResW\neDP-1 Low ResW"
 fi
 
 menu_cmd="rofi -dmenu -i -p Resolutions"
@@ -31,9 +31,9 @@ chosen=$(echo -e "$options" | $menu_cmd)
 
 # Execute chosen action
 case "$chosen" in
-"eDP-1 480p") nwg_displays_480p ;;
-"eDP-1 768p") nwg_displays_768p ;;
-"eDP-0 720p") xrandr_720p ;;
-"eDP-0 900p") xrandr_900p ;;
+"eDP-1 Low ResW") nwg_displays_low_resolution ;;
+"eDP-1 Default ResW") nwg_displays_default_resolution ;;
+"eDP-1 Low ResX") xrandr_low_resolution ;;
+"eDP-1 Default ResX") xrandr_default_resolution ;;
 *) exit 1 ;;
 esac
