4ff58bc6e33ab8149a44c6850436a79540e0dcc6
jhonatanrs10 <jhonatanrs10@hotmail.com>
2026-06-20 14:34:56 -0300

    att

--- jrs/.config/jrs/jrs-scripts-tty.sh
+++ jrs/.config/jrs/jrs-scripts-tty.sh
@@ -2,14 +2,7 @@
 
 setupIwdWiFi() {
 
-	echo "Opções: [1] IWD WIFI [2] NetworkManager"
-	read -r resp
-	case $resp in
-	1)
-		sudo systemctl disable --now NetworkManager
-		sudo systemctl disable --now dhcpcd
-		sudo systemctl enable --now iwd
-
+	iwf_main_file() {
 		sudo tee "/etc/iwd/main.conf" >/dev/null <<EOF
 [General]
 EnableNetworkConfiguration=True
@@ -17,12 +10,27 @@ EnableNetworkConfiguration=True
 RoutePriorityOffset=300
 EnableIPv6=false
 EOF
+	}
+
+	echo "Opções: [1] IWD WIFI [2] NetworkManager [3]ALL ENABLE"
+	read -r resp
+	case $resp in
+	1)
+		iwf_main_file
+		sudo systemctl disable --now NetworkManager
+		sudo systemctl disable --now dhcpcd
+		sudo systemctl enable --now iwd
 		;;
 	2)
 		sudo systemctl disable --now iwd
 		sudo systemctl disable --now dhcpcd
 		sudo systemctl enable --now NetworkManager
-
+		;;
+	3)
+		iwf_main_file
+		sudo systemctl enable --now NetworkManager
+		sudo systemctl enable --now dhcpcd
+		sudo systemctl enable --now iwd
 		;;
 	*)
 		echo "Opção inválida. Nenhuma alteração feita."
